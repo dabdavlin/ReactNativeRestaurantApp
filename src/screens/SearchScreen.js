39e246ee62eabedc50c492aff7fbb546f88339f5
George Daclin <georgetrevord@gmail.com>
2022-06-29 15:44:32 -0500

    second commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "./components/SearchBar";
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
+  return (
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log("term was submitted")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
---
+++ src/screens/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer LlgukgIbEX_9FvjnmaN6USFNzB21GJwMnCbc8qYZAN2JeY-B2utuwfaMhXIxgZV8G0N8R29cN8u-yL9_BjK0xcEqhbY5bX90dkdaYsDQtgIMmYMcZMm-AVjkm-65YnYx",
+  },
+});

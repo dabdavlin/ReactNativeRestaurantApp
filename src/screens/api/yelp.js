7da6dcde7832c9d15ea3bcf3d70dda10f66fedce
George Daclin <georgetrevord@gmail.com>
2022-07-01 13:46:20 -0500

    final commit

--- src/screens/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
-  headers: {
-    Authorization: 'BearerLlgukgIbEX_9FvjnmaN6USFNzB21GJwMnCbc8qYZAN2JeY-B2utuwfaMhXIxgZV8G0N8R29cN8u-yL9_BjK0xcEqhbY5bX90dkdaYsDQtgIMmYMcZMm-AVjkm-65YnYx'
-  }
-});
